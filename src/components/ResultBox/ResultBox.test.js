c5548e8663f472fd033dc7bac6650f8a1b7f6a63
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-22 17:10:53 +0100

    Teat for ResultBox

---
+++ src/components/ResultBox/ResultBox.test.js
@@ -0,0 +1,18 @@
+import ResultBox from './ResultBox';
+import { render,screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+  describe('Component ResultBox', () => {
+    it('should render without crashing', () => {
+        render(<ResultBox from="PLN" to="USD" amount={100} />);
+    });
+
+    it('should render proper info about conversion when PLN -> USD', () => {
+        const amounts = [100, 50, 200, 350];
+        for(let amount of amounts){
+            render(<ResultBox from="PLN" to="USD" amount={amount} />);
+            const output = screen.getByTestId('output');
+            expect(output).toHaveTextContent('PLN 100.00 = $28.57');
+        }
+    });
+});
