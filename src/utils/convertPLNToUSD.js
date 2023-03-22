b3819e64ea14f38a4dfe429ed5b4d638d311a542
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-22 15:04:04 +0100

    Conditionals for PLN

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,21 +1,12 @@
 export const convertPLNToUSD = (PLN) => {
 
-  // Return NaN if input is not a number or a string
-  if (typeof PLN !== 'number' && typeof PLN !== 'string') {
-    return NaN;
-  }
-   // Return NaN if input is an empty string
-   if (typeof PLN === 'string' || PLN === '') {
-    return NaN;
-  }
 
-   // Return "Error" if input is an object, array or function
-   if (typeof PLN === 'object' || typeof PLN === 'function' || Array.isArray(PLN)) {
-    return 'Error';
-  }
-  // Return zero if input is lower than zero
-  if (typeof PLN === 'number' && PLN < 0) {
-    return '$0.00';
+  if (typeof(PLN) === 'string' || PLN === undefined) {
+    return NaN;
+  } else if (typeof(PLN) !== 'number' && typeof(PLN) !== 'string') {
+      return 'Error';
+  } else if (PLN <= 0) {
+    PLN = 0;
   }
   
 
