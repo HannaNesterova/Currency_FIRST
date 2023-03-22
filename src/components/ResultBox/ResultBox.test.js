d3e5fdc31d4497e2cac2250572461925ac3ee452
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-22 18:20:08 +0100

    ResultBox

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -9,11 +9,41 @@ import '@testing-library/jest-dom/extend-expect';
 
     it('should render proper info about conversion when PLN -> USD', () => {
         const amounts = [100, 50, 200, 350];
+
         for(let amount of amounts){
             render(<ResultBox from="PLN" to="USD" amount={amount} />);
             const output = screen.getAllByTestId('resultDiv');
-            expect(output).toHaveTextContent('PLN 100.00 = $28.57');
+            expect(output).toHaveTextContent('PLN 100.00 = $28,57');
             cleanup();
         }
     });
+
+    it('should render proper info about conversion when  USD -> PLN', () => {
+        const amounts = [90, 80, 10, 100];
+
+        for(let amount of amounts){
+        render(<ResultBox from="USD" to="PLN" amount={amount} />);
+        const output = screen.getAllByTestId('resultDiv');
+        expect(output).toHaveTextContent('$90.00 = PLN 315.00');
+        cleanup();
+        }
+    });
+
+    it('should render proper info when same currency is selected in both options', () => {
+        const currencies = ['USD', 'PLN'];
+
+        for(let currency of currencies){
+            render(< ResultBox amount={123} from={currency} to={currency} />);
+            const output = screen.getAllByTestId('resultDiv');
+            if(currency === 'USD'){
+                // eslint-disable-next-line jest/no-conditional-expect
+                expect(output).toHaveTextContent('$123.00 = $123.00');
+            } else {
+              // eslint-disable-next-line jest/no-conditional-expect
+              expect(output).toHaveTextContent('PLN 123.00 = PLN 123.00');
+            }
+            cleanup();
+          }
+
+    })
 });
