0187c32b05a1fcd256cc35a739c0c3e010f17944
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-23 22:42:56 +0100

    ResultBOX

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,55 +1,56 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import ResultBox from './ResultBox';
-import { render,screen, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
-  describe('Component ResultBox', () => {
-    it('should render without crashing', () => {
-        render(<ResultBox from="PLN" to="USD" amount={100} />);
+
+describe('Component ResultBox', () => {
+	it('should render without crashing', () => {
+	    render(< ResultBox amount={100} from='PLN' to='USD' />);
     });
 
-    it('should render proper info about conversion when PLN -> USD', () => {
-        const amounts = [100, 50, 200, 350];
+  it('should render proper info about conversion PLN -> USD', () => {
+    const amounts = [100, 50, 200, 350];
 
-        for(let amount of amounts){
-            render(<ResultBox from="PLN" to="USD" amount={amount} />);
-            const output = screen.getAllByTestId('resultDiv');
-            expect(output).toHaveTextContent('PLN 100.00 = $28,57');
-            cleanup();
-        }
-    });
+    for(let amount of amounts){
+      render(< ResultBox amount={amount} from='PLN' to='USD' />);
+      const resultBox = screen.getByTestId('resultDiv');
+      expect(resultBox).toHaveTextContent(`PLN ${amount}.00 = $${Math.round((amount/3.5)*100)/100}`);
+      cleanup();
+    }
+  });
 
-    it('should render proper info about conversion when  USD -> PLN', () => {
-        const amounts = [90, 80, 10, 100];
+  it('should render proper info about conversion USD -> PLN', () => {
+    const amounts = [90, 80, 10, 100];
 
-        for(let amount of amounts){
-        render(<ResultBox from="USD" to="PLN" amount={amount} />);
-        const output = screen.getAllByTestId('resultDiv');
-        expect(output).toHaveTextContent('$90.00 = PLN 315.00');
-        cleanup();
-        }
-    });
+    for (let amount of amounts){
+      render(< ResultBox amount={amount} from='USD' to='PLN' />);
+      const resultBox = screen.getByTestId('resultDiv');
+      expect(resultBox).toHaveTextContent(`$${amount}.00 = PLN ${amount*3.5}.00`);
+      cleanup();
+    }
+  });
 
-    it('should render proper info when same currency is selected in both options', () => {
-        const currencies = ['USD', 'PLN'];
+  it('should render proper info when same currency is selected in both options', () => {
+    const currencies = ['USD', 'PLN'];
+  
+    for(let currency of currencies){
+      render(< ResultBox amount={123} from={currency} to={currency} />);
+      const resultBox = screen.getByTestId('resultDiv');
+      if(currency === 'USD'){
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(resultBox).toHaveTextContent('$123.00 = $123.00');
+      } else {
+        // eslint-disable-next-line jest/no-conditional-expect
+        expect(resultBox).toHaveTextContent('PLN 123.00 = PLN 123.00');
+      }
+      cleanup();
+    }
+  });
 
-        for(let currency of currencies){
-            render(< ResultBox amount={123} from={currency} to={currency} />);
-            const output = screen.getAllByTestId('resultDiv');
-            if(currency === 'USD'){
-                // eslint-disable-next-line jest/no-conditional-expect
-                expect(output).toHaveTextContent('$123.00 = $123.00');
-            } else {
-              // eslint-disable-next-line jest/no-conditional-expect
-              expect(output).toHaveTextContent('PLN 123.00 = PLN 123.00');
-            }
-            cleanup();
-          }
-    })
-
-    it('should render proper info when amount has negative value', () => {
-        render(< ResultBox amount={-100} from='PLN' to='USD' />);
-        const resultBox = screen.getByTestId('resultDiv');
-        expect(resultBox).toHaveTextContent('Wrong value...');
-
-    })
+  it('should render proper info when amount has negative value', () => {
+    render(< ResultBox amount={-100} from='PLN' to='USD' />);
+      const resultBox = screen.getByTestId('resultDiv');
+    expect(resultBox).toHaveTextContent('Wrong value...');
+  });
 });
+
